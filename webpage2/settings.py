1e0093268aa491f023c5fe285d23f65e5c982901
Shishir Gautam <119574263+ShishirGTM32@users.noreply.github.com>
2024-12-26 09:28:26 +0545

    Update settings.py

--- webpage2/settings.py
+++ webpage2/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-%!(_4%b8k3w^kx2m)$#-hyxfu6(7i00i01$km9b#h6lczvmpcf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='watchhouse.onrender.com').split(',')
 
 
 # Application definition
